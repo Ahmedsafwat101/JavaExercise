f299fb9c95e5c4b9165ddaf69ce0bf73e2ba1512
Ahmed Safwat <AhmedSafwat172@gmail.com>
2021-11-21 14:05:37 +0200

    Task1 Done!

--- Reflections/src/com/company/Main.java
+++ Reflections/src/com/company/Main.java
@@ -1,8 +1,41 @@
 package com.company;
 
+import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
+import java.util.Arrays;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        //Get reference to Test class
+        Class myObjectClass = Test.class;
+
+        System.out.println("Public Fields:");
+
+        // for accessing fields
+        Field[] publicFields = myObjectClass.getFields(); // accessing public fields only
+        Arrays.stream(publicFields).forEach(
+                field -> System.out.println(checkModifierPropertiesType(field.getModifiers()) + "," + field.getType().getSimpleName() + "," + field.getName())
+        );
+
+        System.out.println("\nAll Fields:");
+
+        // for accessing fields
+        Field[] allFields = myObjectClass.getDeclaredFields(); // accessing all fields
+        Arrays.stream(allFields).forEach(
+                field -> System.out.println(checkModifierPropertiesType(field.getModifiers()) + "," + field.getType().getSimpleName() + "," + field.getName())
+        );
+
+    }
+
+    private static String checkModifierPropertiesType(int modifier) {
+        if (Modifier.isProtected(modifier)) {
+            return "Protected";
+        } else if (Modifier.isPrivate(modifier)) {
+            return "Private";
+        } else if (Modifier.isPublic(modifier)) {
+            return "Public";
+        }
+        return "";
     }
 }
---
+++ Reflections/src/com/company/Test.java
@@ -0,0 +1,8 @@
+package com.company;
+
+/**Dummy class for simulating reflections field access**/
+public class Test {
+    private int id;
+    public String name;
+    protected double price;
+}
